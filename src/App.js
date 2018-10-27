df1fe20bfffbb35ff48ef3ed7978993c162df383
valuejoe <chiaohung82@gmail.com>
2018-10-27 16:36:22 +0800

    first react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { NavLink, Switch, Route } from 'react-router-dom';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='app'>
+        <h1>React Router Demo</h1>
+        <Navigation />
+        <Main />
       </div>
     );
   }
 }
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/about'>About</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/contact'>Contact</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const Main = () => (
+  <Switch>
+    <Route exact path='/' component={Home}></Route>
+    <Route exact path='/about' component={About}></Route>
+    <Route exact path='/contact' component={Contact}></Route>
+  </Switch>
+);
+
+const Home = () => (
+  <div className='home'>
+    <h1>歡迎來到我的自介網站</h1>
+    <p>首頁內容</p>
+  </div>
+);
+
+const About = () => (
+  <div className='about'>
+    <h1>關於我</h1>
+    <p>自我介紹內容</p>
+    
+  </div>
+);
+
+const Contact = () => (
+  <div className='contact'>
+    <h1>聯絡我</h1>
+    <p>聯絡資訊</p>
+  </div>
+);
 
 export default App;
